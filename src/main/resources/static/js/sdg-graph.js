c8ae6cc319a9ce9f8cbf3e9f312e22560c0519de
i11007020 <i11007020@gmail.com>
2020-05-17 13:21:22 +0800

    commit

--- src/main/resources/static/js/sdg-graph.js
+++ src/main/resources/static/js/sdg-graph.js
@@ -1439,8 +1439,6 @@ function SDGGraph(data) {
                                 console.log(contractContent);
                                 for( let api in contractContent){
                                     if (contractContent[api]["testResult"]["status"] === "PASS"){
-                                        document.getElementById('serviceCondition').setAttribute("class","badge badge-pill badge-success");
-                                        document.getElementById('serviceCondition').innerText = "PASS";
                                         contractGroup.append("<button class=\"list-group-item list-group-item-action list-group-item-success\" id=\"contract-" + api.substring(1) + "\">" + api + "</button>");
                                     }else {
                                         condition = "false";
@@ -1450,6 +1448,8 @@ function SDGGraph(data) {
                             });
 
                     });
+
+                    console.log(condition)
 
                     if( condition === "false") {
                         document.getElementById('serviceCondition').setAttribute("class","badge badge-pill badge-warning");
