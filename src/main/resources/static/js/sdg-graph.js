2834b903f5d5e394af610cec5a3df3d5f31eeab5
i11007020 <i11007020@gmail.com>
2020-05-17 19:10:03 +0800

    commit

--- src/main/resources/static/js/sdg-graph.js
+++ src/main/resources/static/js/sdg-graph.js
@@ -1478,7 +1478,7 @@ function SDGGraph(data) {
                                     // consumer endpoint
                                     //let link_consumer_endpoint = data.links.filter(lce => (lce.type === REL_OWN) && (lce.source.id === d.id));
 
-                                    let testttt = data.links.filter(lce => (lce.type === REL_OWN) && (lce.source.id === d.id));
+/*                                    let testttt = data.links.filter(lce => (lce.type === REL_OWN) && (lce.source.id === d.id));
                                     console.log(testttt);
                                     let tes;
                                     data.links.filter(lce => (lce.type === REL_OWN) && (lce.source.id === d.id))
@@ -1486,12 +1486,11 @@ function SDGGraph(data) {
                                             let nceTemp = data.nodes.find(nce => (nce.type === REL_HTTPREQUEST) && (nce.source.id === nce2.source.id) && (nce.target.id === node_provider_endpoint.id));
                                             if (nceTemp !== null) {
                                                 tes = nceTemp;
-                                                break;
                                             }else {
                                                 return;
                                             }
                                         });
-                                    console.log(tes);
+                                    console.log(tes);*/
                                     let node_consumer_endpoint = data.links.filter(lce => (lce.type === REL_OWN) && (lce.source.id === d.id))
                                         .forEach(nce2 => {
                                             let nceTemp = data.nodes.find(nce => (nce.type === REL_HTTPREQUEST) && (nce.source.id === nce2.source.id) && (nce.target.id === node_provider_endpoint.id));
