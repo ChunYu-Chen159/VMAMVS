fbcf6a07d760c28f8cc5f5fb663c3b51d2b95b85
i11007020 <i11007020@gmail.com>
2020-06-01 22:12:20 +0800

    commit

--- src/main/java/com/soselab/microservicegraphplatform/services/SleuthService.java
+++ src/main/java/com/soselab/microservicegraphplatform/services/SleuthService.java
@@ -142,7 +142,7 @@ public class SleuthService {
     public String searchZipkin(String appName, String version, int statusCode, long startTime, long endTime, int limit){
         String result = "";
         try {
-            URL url = new URL(Zipkin_V1_BASEPATH+"api/v1/traces?annotationQuery=http.version=" + version + "+and+" + "http.status_code=" + statusCode + "&limit=" + limit + "&startTs=" + startTime + "&endTs=" + endTime + "&serviceName="+appName.toLowerCase()+"&sortOrder=timestamp-desc");
+            URL url = new URL(Zipkin_V1_BASEPATH+"api/v1/traces?annotationQuery=http.version%3D" + version + "%20and%20" + "http.status_code%3D" + statusCode + "&limit=" + limit + "&startTs=" + startTime + "&endTs=" + endTime + "&serviceName="+appName.toLowerCase()+"&sortOrder=timestamp-desc");
             URLConnection urlConnection = url.openConnection();
 
 
