bd73e42dd851c758307aee44b088e17f280754b1
i11007020 <i11007020@gmail.com>
2020-06-02 01:57:14 +0800

    commit

--- src/main/java/com/soselab/microservicegraphplatform/services/RiskService.java
+++ src/main/java/com/soselab/microservicegraphplatform/services/RiskService.java
@@ -101,6 +101,10 @@ public class RiskService {
 
         // 計算RiskValue，放到neo4j存
         for(Service s : ServicesInDB) {
+            System.out.println((double)likelihoodMap.get(s.getAppId()));
+            System.out.println((double)impactMap.get(s.getAppId()));
+            System.out.println((double)likelihoodMap.get(s.getAppId()) * (double)impactMap.get(s.getAppId()));
+
             double riskValue = (double)likelihoodMap.get(s.getAppId()) * (double)impactMap.get(s.getAppId());
             serviceRepository.setRiskValueByAppId(s.getAppId(), riskValue);
         }
