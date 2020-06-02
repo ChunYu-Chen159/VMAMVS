4a8e8c3c3ae2f55b65853447d14202bb45480f68
i11007020 <i11007020@gmail.com>
2020-06-03 03:58:56 +0800

    commit

--- src/main/java/com/soselab/microservicegraphplatform/repositories/neo4j/GeneralRepository.java
+++ src/main/java/com/soselab/microservicegraphplatform/repositories/neo4j/GeneralRepository.java
@@ -55,7 +55,7 @@ public interface GeneralRepository extends Neo4jRepository {
     @Query("MATCH (n {systemName:{systemName}}) WHERE n:Service OR n:Endpoint OR n:Queue " +
             "MATCH (n)-[r]-() WHERE (:Service)-[r:OWN]->(:Endpoint) OR ()-[r:HTTP_REQUEST]->() OR ()-[r:AMQP_PUBLISH]->() OR ()-[r:AMQP_SUBSCRIBE]->() OR ()-[r:NEWER_PATCH_VERSION]->() " +
             "WITH collect(DISTINCT n) as ns, collect(DISTINCT r) as rs " +
-            "WITH [node in ns | node {.*, highRiskCondition:highRiskCondition(node), id:id(node), labels:labels(node)}] as nodes, " +
+            "WITH [node in ns | node {.*, id:id(node), labels:labels(node)}] as nodes, " +
             "[rel in rs | rel {.*, type:type(rel), " +
             "source:id(startNode(rel)), target:id(endNode(rel))}] as rels " +
             "RETURN apoc.convert.toJson({nodes:nodes, links:rels})")
