85daa49b393dbf4e594976336e78c9625154e41d
i11007020 <i11007020@gmail.com>
2020-06-03 19:00:54 +0800

    commit

--- src/main/resources/static/js/sdg-graph.js
+++ src/main/resources/static/js/sdg-graph.js
@@ -1979,6 +1979,9 @@ function SDGGraph(data) {
                         });
                 }
                 form.classList.add('was-validated');
+
+                update(emptyData);
+                update(graphData);
             });
 
         });
