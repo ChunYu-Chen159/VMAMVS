c18f2756c32eb8cf8ab41716a9a852b18f556202
i11007020 <i11007020@gmail.com>
2020-06-12 10:19:12 +0800

    commit

--- src/main/resources/static/js/sdg-graph.js
+++ src/main/resources/static/js/sdg-graph.js
@@ -1718,10 +1718,10 @@ function SDGGraph(data) {
                                 console.log("groovy: " + groovy);
                                 let consumerService = groovy.replace(".groovy","");
                                 monitorErrorGroup.append("<h3 id=\"" + consumerService + "-errors\" class=\"card-monitorError\">" + JSON.stringify(consumerService).toUpperCase() + "</h3>");
-                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error500\"><h4 class=\"card-monitorError\">" + "Error Detect (500)" + "</h4></div>");
-                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error502\"><h4 class=\"card-monitorError\">" + "Error Detect (502)" + "</h4></div>");
-                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error503\"><h4 class=\"card-monitorError\">" + "Error Detect (503)" + "</h4></div>");
-                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error504\"><h4 class=\"card-monitorError\">" + "Error Detect (504)" + "</h4></div>");
+                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error500\" style=\"display:none;\"><h4 class=\"card-monitorError\">" + "Error Detect (500)" + "</h4></div>");
+                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error502\" style=\"display:none;\"><h4 class=\"card-monitorError\">" + "Error Detect (502)" + "</h4></div>");
+                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error503\" style=\"display:none;\"><h4 class=\"card-monitorError\">" + "Error Detect (503)" + "</h4></div>");
+                                monitorErrorGroup.append("<div id=\"" + consumerService + "-error504\" style=\"display:none;\"><h4 class=\"card-monitorError\">" + "Error Detect (504)" + "</h4></div>");
                             }
 
                             for(let everyError = 0;everyError < Object.keys(json).length; everyError++){
@@ -1743,7 +1743,7 @@ function SDGGraph(data) {
                                 let iddd2 = "error-" + index;
 
                                 document.getElementById(iddd).innerHTML += "<button class=\"list-group-item list-group-item-action list-group-item-danger\" id=\"" + iddd2 + "\" >" + "error-" + index + "</button>";
-
+                                document.getElementById(iddd).style.display = "block";
 
                             }
 
