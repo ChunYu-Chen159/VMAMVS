dbfc15abaa1bed5b5e97703b7b827a99648d87e2
i11007020 <i11007020@gmail.com>
2020-07-03 21:04:44 +0800

    commit

--- src/main/java/com/soselab/microservicegraphplatform/services/MonitorService.java
+++ src/main/java/com/soselab/microservicegraphplatform/services/MonitorService.java
@@ -131,7 +131,10 @@ public class MonitorService {
 
         allMonitorErrorList.replace(systemName, monitorErrors);*/
 
-        System.out.println("allMonitorErrorList.get(systemName): " + allMonitorErrorList.get(systemName).get(0).isMonitorError_testedPASS());
+        System.out.println("allMonitorErrorList.getOrDefault(systemName, null);: " + allMonitorErrorList.getOrDefault(systemName, null));
+        System.out.println("size(): " + allMonitorErrorList.get(systemName).size());
+
+//        System.out.println("allMonitorErrorList.get(systemName): " + allMonitorErrorList.get(systemName).get(0).isMonitorError_testedPASS());
 
 
         for(Service s : ServicesInDB) {
