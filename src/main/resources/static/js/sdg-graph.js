8ea14aa5322e343308d114339dce69bee9df308f
i11007020 <i11007020@gmail.com>
2020-07-22 23:48:52 +0800

    commit

--- src/main/resources/static/js/sdg-graph.js
+++ src/main/resources/static/js/sdg-graph.js
@@ -2287,7 +2287,7 @@ function SDGGraph(data) {
                                         let groovyName = d.appName.toLowerCase() + ".groovy";
                                         if(!contractContent[groovyName] || !contractContent[groovyName][targetApi]){
                                             console.log("12332132123132132131");
-                                            $('#contractMissing-' + targetService.toUpperCase()).after("<button class=\"list-group-item list-group-item-action list-group-item-warning\" id=\"contractMissing-" + json["info"]["title"].toUpperCase() + "-" + api.substring(1)  + "\">" + api + "</button>");
+                                            $('#contractMissing-' + targetService.toUpperCase()).after("<button class=\"list-group-item list-group-item-action list-group-item-warning\" id=\"contractMissing-" + json["info"]["title"].toUpperCase() + "-" + targetApi.substring(1)  + "\">" + targetApi + "</button>");
                                             $('#contractMissing-' + targetService.toUpperCase()).show();
                                         }
                                         if(!contractContent[groovyName] || !contractContent[groovyName][targetApi]){
